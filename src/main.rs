22895b4da8ca374d79a4eb8ec098723b4b5ef57a
Di Saber <marc.a.benamou@gmail.com>
2023-12-15 10:51:21 -0600

    Add a generic test shader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod vector3;
 
+use std::ffi::CString;
+
 use glfw::Context;
 use vector3::{FlattenVector3, Vector3};
 
@@ -13,7 +15,7 @@ fn main() {
         glfw::ContextCreationApi::Native,
     ));
 
-    let (mut window, events) = glfw
+    let (mut window, _events) = glfw
         .create_window(960, 540, "OpenGL Test", glfw::WindowMode::Windowed)
         .unwrap();
 
@@ -43,17 +45,116 @@ fn main() {
     ]
     .flatten();
 
-    let mut vbo: u32 = 0;
+    let mut vbo = 0u32;
+    let vertex_shader_source = CString::new(include_str!("shaders/triangle.vert")).unwrap();
+    let fragment_shader_source = CString::new(include_str!("shaders/triangle.frag")).unwrap();
 
     unsafe {
         gl::GenBuffers(1, &mut vbo);
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(
             gl::ARRAY_BUFFER,
-            (triangle_vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-            triangle_vertices.as_ptr() as *const gl::types::GLvoid,
+            (triangle_vertices.len() * std::mem::size_of::<f32>()) as isize,
+            triangle_vertices.as_ptr() as *const std::ffi::c_void,
             gl::STATIC_DRAW,
-        )
+        );
+
+        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(
+            vertex_shader,
+            1,
+            &vertex_shader_source.as_ptr(),
+            std::ptr::null(),
+        );
+        gl::CompileShader(vertex_shader);
+
+        let mut success = 0i32;
+        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+
+        if success == 0 {
+            let mut error_length = 0i32;
+            gl::GetShaderiv(vertex_shader, gl::INFO_LOG_LENGTH, &mut error_length);
+
+            let error = CString::from_vec_unchecked(
+                [b' ']
+                    .iter()
+                    .cycle()
+                    .take(error_length as usize)
+                    .cloned()
+                    .collect::<Vec<u8>>(),
+            );
+            gl::GetShaderInfoLog(
+                vertex_shader,
+                error_length,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut i8,
+            )
+        }
+
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(
+            fragment_shader,
+            1,
+            &fragment_shader_source.as_ptr(),
+            std::ptr::null(),
+        );
+        gl::CompileShader(fragment_shader);
+
+        let mut success = 0i32;
+        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
+
+        if success == 0 {
+            let mut error_length = 0i32;
+            gl::GetShaderiv(fragment_shader, gl::INFO_LOG_LENGTH, &mut error_length);
+
+            let error = CString::from_vec_unchecked(
+                [b' ']
+                    .iter()
+                    .cycle()
+                    .take(error_length as usize)
+                    .cloned()
+                    .collect::<Vec<u8>>(),
+            );
+            gl::GetShaderInfoLog(
+                fragment_shader,
+                error_length,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut i8,
+            )
+        }
+
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        let mut success = 0i32;
+        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
+
+        if success == 0 {
+            let mut error_length = 0i32;
+            gl::GetProgramiv(fragment_shader, gl::INFO_LOG_LENGTH, &mut error_length);
+
+            let error = CString::from_vec_unchecked(
+                [b' ']
+                    .iter()
+                    .cycle()
+                    .take(error_length as usize)
+                    .cloned()
+                    .collect::<Vec<u8>>(),
+            );
+            gl::GetProgramInfoLog(
+                fragment_shader,
+                error_length,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut i8,
+            )
+        }
+
+        gl::DetachShader(shader_program, vertex_shader);
+        gl::DetachShader(shader_program, fragment_shader);
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
     }
 
     while !window.should_close() {
