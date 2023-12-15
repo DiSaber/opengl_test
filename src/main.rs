50ea446425a9ba23960042d4fdb5b003e9ef234c
Di Saber <marc.a.benamou@gmail.com>
2023-12-14 19:43:13 -0600

    Swap from SDL2 to GLFW

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use glfw::Context;
+
+extern crate gl;
+extern crate glfw;
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+    glfw.window_hint(glfw::WindowHint::ContextCreationApi(
+        glfw::ContextCreationApi::Native,
+    ));
+
+    let (mut window, events) = glfw
+        .create_window(960, 540, "OpenGL Test", glfw::WindowMode::Windowed)
+        .unwrap();
+
+    window.make_current();
+    gl::load_with(|s| window.get_proc_address(s));
+
+    window.set_framebuffer_size_callback(|_, width, height| unsafe {
+        gl::Viewport(0, 0, width, height);
+    });
+
+    while !window.should_close() {
+        if window.get_key(glfw::Key::Escape) == glfw::Action::Press {
+            window.set_should_close(true);
+        }
+
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.swap_buffers();
+        glfw.poll_events();
+    }
 }
